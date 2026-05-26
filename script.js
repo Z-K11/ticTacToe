5ecb9848097013f960d83c635636b32fddcc14f5
theZK11 <zulqarnainbazidkhail@gmail.com>
2026-05-26 14:37:36 +0500

    implement factory functions for player creation

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 console.log("Hello World");
+function createGame()
+{
+
+}
+function createPlayer(name)
+{
+    let wins=0;
+    let losses=0;
+    return {
+        player:name,
+        addWin()
+        {
+            wins++;
+            console.log(`${name} wins!`);
+        },
+        addLoss()
+        {   
+            losses++;
+            console.log(`${name} losses!`);
+        },
+        displayScore()
+        {
+            console.log(`${name} has losses = ${losses} and wins = ${wins}`);
+        }
+    }
+}
+const player_1 = createPlayer('ZK11');
+const player_2 = createPlayer('Maratib');
